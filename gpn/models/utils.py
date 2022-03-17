1ee1e352148751d5977e9c17815bf4e1d691d1f7
sachaStewart <sachalevy26@gmail.com>
2022-03-17 09:47:43 -0400

    add more message passing steps

--- gpn/models/utils.py
+++ gpn/models/utils.py
@@ -4,7 +4,7 @@ from .model import Model
 
 
 def create_model(params: ModelConfiguration) -> Model:
-    """ initialize model wih controlled randomness through iterative initializations based on params.init_no
+    """initialize model wih controlled randomness through iterative initializations based on params.init_no
 
     Args:
         params (ModelConfiguration): all values specifying the model's configuration
@@ -14,6 +14,7 @@ def create_model(params: ModelConfiguration) -> Model:
     """
 
     model = getattr(sys.modules[__package__], params.model_name)
+    print(f"Creating model {params.model_name}, found here {model}")
 
     for _ in range(params.init_no):
         m = model(params)
